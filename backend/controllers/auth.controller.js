e2b29b80d3abb599a005702f1516f16aa7c9cb56
bhargavnama <bhargavnama143@gmail.com>
2025-01-25 22:28:29 +0530

    1-project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+    res.json({message: 'You hit the signup route'});
+};
+
+export const login = async (req, res) => {
+    res.json({message: 'You hit the login route'});
+};
+
+export const logout = async (req, res) => {
+    res.json({message: 'You hit the logout route'});
+};
