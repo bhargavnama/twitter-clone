42d88e3de377a5f20e15900f451cf96afc72bbaa
bhargavnama <bhargavnama143@gmail.com>
2025-01-30 17:10:57 +0530

    3- Profile page posts and user info fetched

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -161,7 +161,7 @@ export const getAllPosts = async (req, res) => {
 };
 
 export const getLikedPosts = async (req, res) => {
-  const userId = req.user._id;
+  const userId = req.userId;
   try {
     const user = await User.findById(userId);
     if (!user) {
